80b0a960642437c10b16424478af2cfd8e943ce5
Thomas Noriega <trnoriega@users.noreply.github.com>
2018-11-09 15:19:20 -0800

    Modify simple_request to work with the new server.

--- simple_request.py
+++ simple_request.py
@@ -6,23 +6,22 @@ import requests
 
 # initialize the Keras REST API endpoint URL along with the input
 # image path
-KERAS_REST_API_URL = "http://localhost:5000/predict"
-IMAGE_PATH = "dog.jpg"
+KERAS_REST_API_URL = 'http://localhost:5000/predict'
+IMAGE_PATH = 'Dodge-Ram_Pickup_3500-2009.jpg'
 
 # load the input image and construct the payload for the request
-image = open(IMAGE_PATH, "rb").read()
-payload = {"image": image}
+image = open(IMAGE_PATH, 'rb').read()
+payload = {'image': image}
 
 # submit the request
 r = requests.post(KERAS_REST_API_URL, files=payload).json()
 
 # ensure the request was sucessful
-if r["success"]:
+if r['success']:
 	# loop over the predictions and display them
-	for (i, result) in enumerate(r["predictions"]):
-		print("{}. {}: {:.4f}".format(i + 1, result["label"],
-			result["probability"]))
+	for (i, result) in enumerate(r['predictions']):
+		print(f'{i + 1}. {result["label"]}: {result["probability"]:.4f}')
 
 # otherwise, the request failed
 else:
-	print("Request failed")
+	print('Request failed')
